a4d16e1d3f66529d5ad60f168b1db9ff5da034d6
Marianna <marianna.budnikova@gmail.com>
2015-06-20 14:41:02 -0600

    Initial commit

---
+++ desktop/src/com/whimsied/vampification/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.whimsied.vampification.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.whimsied.vampification.Vampification;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Vampification(), config);
+	}
+}
